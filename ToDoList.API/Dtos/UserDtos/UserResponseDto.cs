32daa11e251321791167bb41410d1716797cbb20
hakamqadi <hakam.h.qadi@gmail.com>
2025-09-01 20:32:02 +0300

    update UserResponseDto

--- ToDoList.API/Dtos/UserDtos/UserResponseDto.cs
+++ ToDoList.API/Dtos/UserDtos/UserResponseDto.cs
@@ -3,7 +3,8 @@ using ToDoList.API.Data;
 namespace ToDoList.API.Dtos.UserDtos;
 
 public record class UserResponseDto(
-    int Id,
+    string Id,
+    // int Id,
     string FullName,
     UserRole Role,
     string Token
