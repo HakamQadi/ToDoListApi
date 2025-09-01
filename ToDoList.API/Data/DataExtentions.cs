0bf3832465328cd3e6cbd60e4bf36d4abefeb6d5
hakamqadi <hakam.h.qadi@gmail.com>
2025-09-01 19:18:42 +0300

    implement swagger and db

--- ToDoList.API/Data/DataExtentions.cs
+++ ToDoList.API/Data/DataExtentions.cs
@@ -1,14 +1,15 @@
-// using System;
+using System;
+using Microsoft.EntityFrameworkCore;
 
-// namespace ToDoList.API.Data;
+namespace ToDoList.API.Data;
 
-// public class DataExtentions
-// {
-//     public static void AddDatabase(this IServiceCollection services, IConfiguration config)
-//     {
-//         var connectionString = config.GetConnectionString("DefaultConnection");
+public static class DataExtentions
+{
+    public static void AddDatabase(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("DefaultConnection");
 
-//         services.AddDbContext<FootballContext>(options =>
-//             options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-//     }
-// }
+        services.AddDbContext<Context>(options =>
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    }
+}
--- ToDoList.API/Program.cs
+++ ToDoList.API/Program.cs
@@ -1,6 +1,20 @@
+using ToDoList.API.Data;
+using ToDoList.API.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FootballPlayersAPI v1");
+});
+
+app.MapUserEndpoints();
 
 app.Run();
