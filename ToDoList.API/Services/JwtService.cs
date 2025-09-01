1ef687a2307bf38f57b794c43414cc110408c0ed
hakamqadi <hakam.h.qadi@gmail.com>
2025-09-01 20:31:04 +0300

    update JwtService

--- ToDoList.API/Services/JwtService.cs
+++ ToDoList.API/Services/JwtService.cs
@@ -1,37 +1,34 @@
-using System;
-using ToDoList.API.Data;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
+using ToDoList.API.Data;
+
 namespace ToDoList.API.Services;
 
 public class JwtService
 {
     private readonly IConfiguration _config;
-    public JwtService(IConfiguration config)
-    {
-        _config = config;
-    }
+
+    public JwtService(IConfiguration config) => _config = config;
 
     public string GenerateToken(User user)
     {
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
         var claims = new[]
         {
             new Claim(JwtRegisteredClaimNames.Sub, user.Id),
-            new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
-            new Claim(ClaimTypes.Email, user.Email),
-            new Claim(ClaimTypes.Role, user.Role.ToString())
+            new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+            new Claim("role", user.Role.ToString())
         };
 
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
         var token = new JwtSecurityToken(
-            issuer: _config["Jwt:Issuer"],
-            audience: _config["Jwt:Audience"],
-            claims: claims,
-            expires: DateTime.UtcNow.AddMinutes(double.Parse(_config["Jwt:ExpireMinutes"]!)),
+            _config["Jwt:Issuer"],
+            _config["Jwt:Audience"],
+            claims,
+            expires: DateTime.UtcNow.AddHours(2),
             signingCredentials: creds
         );
 
